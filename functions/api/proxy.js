3ff87472c7bcf3990a91e443b294d5abe04237f7
Ryan <gyanreyer@gmail.com>
2025-04-30 12:50:44 -0400

    Cache proxied responses to reduce duplicate requests across different clients

--- functions/api/proxy.js
+++ functions/api/proxy.js
@@ -3,6 +3,67 @@
  */
 
 /**
+ * @param {Request} proxyRequest
+ * @param {Headers} responseHeaders
+ * @returns {Promise<Response>}
+ */
+const getProxiedResponse = async (proxyRequest, responseHeaders) => {
+  try {
+    const proxiedResponse = await fetch(proxyRequest);
+
+    if (proxiedResponse.ok) {
+      const requestEtag = proxyRequest.headers.get("If-None-Match");
+
+      // Some servers include an Etag/Last-Modified header but still return a full 200 response
+      // even if the header indicates the resource is unchanged. In this case, we'll double-check
+      // the headers to see if we can return a 304 response to the client and save some bandwidth.
+      if (requestEtag && proxiedResponse.headers.get("Etag") === requestEtag) {
+        proxiedResponse.body.cancel();
+        return new Response(null, {
+          status: 304,
+          responseHeaders,
+        });
+      }
+
+      const requestIfModifiedSince =
+        proxyRequest.headers.get("If-Modified-Since");
+
+      if (requestIfModifiedSince) {
+        const responseLastModified =
+          proxiedResponse.headers.get("Last-Modified");
+        if (
+          responseLastModified &&
+          new Date(responseLastModified).getTime() <=
+            new Date(requestIfModifiedSince).getTime()
+        ) {
+          proxiedResponse.body.cancel();
+          return new Response(null, {
+            status: 304,
+            responseHeaders,
+          });
+        }
+      }
+    }
+
+    proxiedResponse.headers.forEach((value, key) => {
+      // Apply the proxied response headers to the responseHeaders object
+      responseHeaders.append(key, value);
+    });
+
+    return new Response(proxiedResponse.body, {
+      status: proxiedResponse.status,
+      statusText: proxiedResponse.statusText,
+      headers: responseHeaders,
+    });
+  } catch (e) {
+    return new Response(`Failed to proxy request to ${proxyURL}`, {
+      status: 500,
+      responseHeaders,
+    });
+  }
+};
+
+/**
  * @type {PagesFunction}
  */
 export const onRequest = async (context) => {
@@ -57,66 +118,36 @@ export const onRequest = async (context) => {
     );
   }
 
-  try {
-    const requestEtag = context.request.headers.get("If-None-Match");
-    const requestIfModifiedSince =
-      context.request.headers.get("If-Modified-Since");
-    const proxyRequestHeaders = new Headers({
+  const cache = await caches.open("proxy-cache");
+  let cachedResponse = await cache.match(proxyURL);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  const requestEtag = context.request.headers.get("If-None-Match");
+  const requestIfModifiedSince =
+    context.request.headers.get("If-Modified-Since");
+
+  const proxyRequest = new Request(proxyURL, {
+    method: "GET",
+    headers: new Headers({
       "If-None-Match": requestEtag,
       "If-Modified-Since": requestIfModifiedSince,
       "User-Agent": context.env.USER_AGENT,
-    });
+    }),
+  });
 
-    const proxiedResponse = await fetch(proxyURL, {
-      method: context.request.method,
-      headers: proxyRequestHeaders,
-      body: context.request.body,
-    });
+  const proxiedResponse = await getProxiedResponse(
+    proxyRequest,
+    responseHeaders
+  );
 
-    if (proxiedResponse.ok) {
-      // Some servers include an Etag/Last-Modified header but still return a full 200 response
-      // even if the header indicates the resource is unchanged. In this case, we'll double-check
-      // the headers to see if we can return a 304 response to the client and save some bandwidth.
-
-      if (requestEtag && proxiedResponse.headers.get("Etag") === requestEtag) {
-        proxiedResponse.body.cancel();
-        return new Response(null, {
-          status: 304,
-          responseHeaders,
-        });
-      }
-
-      if (requestIfModifiedSince) {
-        const responseLastModified =
-          proxiedResponse.headers.get("Last-Modified");
-        if (
-          responseLastModified &&
-          new Date(responseLastModified).getTime() <=
-            new Date(requestIfModifiedSince).getTime()
-        ) {
-          proxiedResponse.body.cancel();
-          return new Response(null, {
-            status: 304,
-            responseHeaders,
-          });
-        }
-      }
-    }
-
-    proxiedResponse.headers.forEach((value, key) => {
-      // Apply the proxied response headers to the responseHeaders object
-      responseHeaders.append(key, value);
-    });
-
-    return new Response(proxiedResponse.body, {
-      status: proxiedResponse.status,
-      statusText: proxiedResponse.statusText,
-      headers: responseHeaders,
-    });
-  } catch (e) {
-    return new Response(`Failed to proxy request to ${proxyURL}`, {
-      status: 500,
-      responseHeaders,
-    });
+  if (proxiedResponse.ok) {
+    // Write the proxied response to the cache; it will expire in 1 hour
+    cachedResponse = proxiedResponse.clone();
+    cachedResponse.headers.set("Cache-Control", "max-age=3600");
+    await cache.put(proxyURL, cachedResponse);
   }
+
+  return proxiedResponse;
 };
